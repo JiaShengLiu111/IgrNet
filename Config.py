b01c98f852f23afbbf61ed616950fc232b4ead1c
JiaShengLiu111 <1733546743@qq.com>
2020-02-17 15:02:59 +0800

    Modify README.md

--- Config.py
+++ Config.py
@@ -4,44 +4,35 @@ import sys
 stdi,stdo,stde=sys.stdin,sys.stdout,sys.stderr 
 reload(sys) 
 sys.stdin,sys.stdout,sys.stderr=stdi,stdo,stde 
-sys.setdefaultencoding('utf-8')   # 解决汉字编码问题
+sys.setdefaultencoding('utf-8')
 
 
-# the parameters of regular
-weight_decay_rate = 1e-5  # L2正则化权重衰减系数
+# The parameters of L2 regular and dropout
+weight_decay_rate = 1e-5  
 dropout_rate = 0.2  
 
-# the parameters of train process
+# The parameters of train process
 batch_size = 32
 epochs = 160  # the total epochs of the total train_set
-firstPred = True  # 表示在首次训练之前是否计算模型初始的损失函数
-saveModelPath = '/notebooks/17_LJS/model2019/20190525_TBS三分类加入淡染样本/20190611_TBS三分类模型对比/SpliceNet新探索2_20190726/SpliceNet_增大图片拼接数量_单个图片SoftMax_按照不同样本拼接图片/model_1'  # 模型的保存路径
-train_reDir = "info/info.txt"  # 训练过程输出信息重定向文件
-test_redir = "info_test/info.txt"  # 预测过程输出信息重定向文件
+firstPred = True  # evaluate the model at initial state or not
+saveModelPath = 'xxx/model_1'  # save model path
+train_reDir = "log/log.txt"  # the log file of training process
 
-# !!！注意!!！：一旦loadmodel_path则会加载预训练模型
-loadmodel_path = '/notebooks/17_LJS/model2019/20190525_TBS三分类加入淡染样本/20190611_TBS三分类模型对比/SpliceNet新探索2_20190726/SpliceNet_增大图片拼接数量_单个图片SoftMax_按照不同样本拼接图片/'  # 预训练模型路径
-loadmodel_name = 'model_1.meta'  # 预训练模型名称
-saveStepEpochRate = 1  # 表示每训练saveStepEpochRate*epoch时，判断并保存一次最优模型
+# The pretrained model
+loadmodel_path = 'xxx'  # path of pretrained model
+loadmodel_name = 'model_1.meta'  # name of pretrained model
+saveStepEpochRate = 1  # evaluate the model every saveStepEpochRate EPOCH
 
-# the parameters of ptimizer
-lr = 1e-3  # 初始学习率
-lr_decay_times = 8  # （训练过程中）学习率衰减次数
-lr_decay_rate = 0.94  # 学习率指数衰减率
+# The parameters of optimizer
+lr = 1e-3  # init learning rate
+lr_decay_times = 8  # the times of lr exponential decay
+lr_decay_rate = 0.94  # the rate of lr exponential decay
 
-# 拼接图像每行细胞个数、每列细胞个数
-image_column=5
-image_row=5
-image_size = 112  # 单个宫颈细胞图像进行数据增强后的大小
+# The parameters of IgrNet
+image_column=5  # the column of spliced image
+image_row=5  # the row of spliced image
+image_size = 112  # the size of cell patches
+class_num = 3  # the number of category
+junk_index = 0  # the category of padding images
 
-junk_index = 0  # 表示在进行拼接时，当某一个样本的细胞数量不足以拼接时，背景区域当做成哪一个类别。
-
-class_num = 3
-
-
-
-# 修改备注： 
-# 注意：getSingleTrainPic函数中取消了对列表混合打乱操作。！！！！！！！！！！！！！！！
-
-  
 
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # IgrNet
 A novel deep network named IgrNet, which can model the relationships of pathological features of cervical cells from the same specimen by a technology of intra-group reference.
+
+
+
+###  **The framework of IgrNet:**
+
+![](img/MainFrameAndLoss.png)
+
+
+
+### **Usage:**
+
+1. **Modify  configuration file [Config.py](https://github.com/JiaShengLiu111/IgrNet/blob/master/Config.py) according to your own needs;**
+
+2. **Clear the folder <u>log</u>;**
+
+3. **Modify the <u>run.ipynb</u>: change the dataset file path as follows:**
+
+   ![1581922432169](assets/1581922432169.png)
+
+4. **Run the file of <u>run.ipynb</u> ;**
+
+5. **View the experimental results as follows:**
+
+   ![1581922045041](assets/cost.jpg) ![1581922045041](assets/perform.jpg)
+
